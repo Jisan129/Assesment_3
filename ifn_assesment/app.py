8440fdd70360661e6a8369d5936c5aa597624dcb
Jisan <jisananam1228@gmail.com>
2025-10-18 21:33:59 +1000

    log in and registration

--- ifn_assesment/app.py
+++ ifn_assesment/app.py
@@ -1,52 +1,8 @@
-# from flask import Flask,render_template
-# from werkzeug.security import generate_password_hash, check_password_hash
-#
-# app = Flask(__name__)
-#
-#
-# @app.route('/')
-# def home():
-#     hashed_password = generate_password_hash('mypassword123')
-#
-#     return render_template('index.html')
-#
-#
-# @app.route('/register', methods=['GET', 'POST'])
-# def register():
-#     msg = ''
-#     if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
-#         username = request.form['username']
-#         password = request.form['password']
-#         email = request.form['email']
-#
-#         cursor = mysql.connection.cursor()
-#         cursor.execute('SELECT * FROM users WHERE username = %s', (username,))
-#         account = cursor.fetchone()
-#
-#         if account:
-#             msg = 'Account already exists!'
-#         elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-#             msg = 'Invalid email address!'
-#         elif not re.match(r'[A-Za-z0-9]+', username):
-#             msg = 'Username must contain only characters and numbers!'
-#         elif not username or not password or not email:
-#             msg = 'Please fill out the form!'
-#         else:
-#             hashed_password = generate_password_hash(password)
-#             cursor.execute('INSERT INTO users (username, email, password) VALUES (%s, %s, %s)',
-#                            (username, email, hashed_password))
-#             mysql.connection.commit()
-#             cursor.close()
-#             msg = 'You have successfully registered!'
-#             return redirect(url_for('login'))
-#
-#     return render_template('register.html', msg=msg)
-from flask import Flask, request, redirect, session, render_template
+from flask import Flask, request, redirect, session, render_template, url_for, flash
 from flask_mysqldb import MySQL
-from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
-app.secret_key = 'change_this_secret_key'
+app.secret_key = 'change_this_secret_key'  # Change this to a random secret key
 
 # MySQL Config
 app.config['MYSQL_HOST'] = 'localhost'
@@ -56,84 +12,107 @@ app.config['MYSQL_DB'] = 'flask_app'
 
 mysql = MySQL(app)
 
+
+# Home route
 @app.route('/')
 def home():
-    name = "Jishan"
-    return render_template('index.html', name=name)
+    # Check if user is logged in
+    if 'loggedin' in session:
+        username = session['username']
+        return render_template('index.html', username=username)
+    return render_template('index.html')
 
+
+# Registration route
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    msg = ''
+
+    if request.method == 'POST':
+        # Get form data
+        username = request.form.get('username')
+        password = request.form.get('password')
+        email = request.form.get('email')
+
+        # Validation
+        if not username or not password:
+            msg = 'Please fill out the form completely!'
+        else:
+            # Check if account already exists
+            cursor = mysql.connection.cursor()
+            cursor.execute('SELECT * FROM users WHERE username = %s', (username,))
+            account = cursor.fetchone()
+
+            if account:
+                msg = 'Account already exists!'
+            else:
+                # Insert new user into database (plain text password)
+                cursor.execute('INSERT INTO users (username, password, email) VALUES (%s, %s, %s)',
+                               (username, password, email))
+                mysql.connection.commit()
+                cursor.close()
+
+                msg = 'You have successfully registered! Please login.'
+                return redirect(url_for('login'))
+
+    return render_template('register.html', msg=msg)
+
+
+# Login route
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     msg = ''
+
     if request.method == 'POST':
+        # Get form data
         username = request.form.get('username')
         password = request.form.get('password')
+        print(username)
+        print(password)
+        cursor = mysql.connection.cursor()
+        # Check if account exists
+        cursor = mysql.connection.cursor()
+        cursor.execute('SELECT * FROM users WHERE username = %s', (username,))
+        account = cursor.fetchone()
+        cursor.close()
+        print(account)
+        print(account[2])
+        # Compare plain text passwords directly
+        if account and account[3] == password:
+            # Password is correct - create session
+            session['loggedin'] = True
+            session['id'] = account[0]
+            session['username'] = account[1]
 
-        cur = mysql.connection.cursor()
-        cur.execute('SELECT * FROM users WHERE username = %s', (username,))
-        user = cur.fetchone()
-        cur.close()
-
-        if user and check_password_hash(user[3], password):
-            session['user'] = username
-            session['user_id'] = user[0]
-            flash('Login successful!', 'success')
+            # Redirect to home page
             return redirect(url_for('home'))
         else:
-            msg = 'Invalid credentials!'
+            msg = 'Incorrect username or password!'
 
     return render_template('login.html', msg=msg)
 
 
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        email = request.form['email']
-        password = request.form['password']
-
-        hashed = generate_password_hash(password)
-
-        cur = mysql.connection.cursor()
-        cur.execute('INSERT INTO users (username, email, password) VALUES (%s, %s, %s)',
-                    (username, email, hashed))
-        mysql.connection.commit()
-        cur.close()
-
-        return '<p style="color:green">Registered! <a href="/login">Login here</a></p>'
-
-    return register_form()
-
-
+# Logout route
 @app.route('/logout')
 def logout():
-    session.pop('user', None)
-    return redirect('/login')
+    # Remove session data
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+
+    return redirect(url_for('login'))
 
 
-def login_form():
-    return '''x
-        <h2>Login</h2>
-        <form method="post">
-            <input name="username" placeholder="Username" required><br><br>
-            <input name="password" type="password" placeholder="Password" required><br><br>
-            <button>Login</button>
-        </form>
-        <p><a href="/register">Register</a></p>
-    '''
+# Protected route example
+@app.route('/profile')
+def profile():
+    # Check if user is logged in
+    if 'loggedin' in session:
+        username = session['username']
+        return render_template('profile.html', username=username)
 
-
-def register_form():
-    return '''
-        <h2>Register</h2>
-        <form method="post">
-            <input name="username" placeholder="Username" required><br><br>
-            <input name="email" placeholder="Email" type="email" required><br><br>
-            <input name="password" type="password" placeholder="Password" required><br><br>
-            <button>Register</button>
-        </form>
-        <p><a href="/login">Login</a></p>
-    '''
+    # User is not logged in, redirect to login page
+    return redirect(url_for('login'))
 
 
 if __name__ == '__main__':
